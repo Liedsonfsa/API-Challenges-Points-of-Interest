a105434794a2cf480670c570b112a0e8d14a9381
Liedsonfsa <fabricoliedson@gmail.com>
2025-04-06 08:41:30 -0300

    feat: adding the initial structure

---
+++ internal/database/init.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func initDatabase() (*sql.DB, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	stringConnection := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	db, err := sql.Open("mysql", stringConnection)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
